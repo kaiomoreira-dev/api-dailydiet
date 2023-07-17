b6a0f1370ea5ab93cc6ee941ce0575261a5a8534
Kaio Moreira <kaio-dev@outlook.com>
2023-07-17 16:59:40 -0300

    add cors for api

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
 import cookie from '@fastify/cookie'
 import { snacksRoutes } from './routes/snack'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(cors)
 
 app.register(cookie)
 
